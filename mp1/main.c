1920273ec91926a5ffe53b015842a1ad87abbe9d
jintian_song <jintian4@illinois.edu>
2022-01-24 00:36:38 -0600

    Start

--- mp1/main.c
+++ mp1/main.c
@@ -7,6 +7,7 @@
 int main() {
   // Feel free to delete all of this and use this function in any way you want:
   printf("Currently, `main.c` does nothing but has all of your code included.\n");
+  printf("stupid as fuck. \n");
   printf("- Edit `main.c` to test your functions within the context of `main`.\n");
   printf("- View the `test` directory to view the test cases defiend for this MP.\n");
   printf("- Run `make test` and `./test` to run the test suite.\n");
